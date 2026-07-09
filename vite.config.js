923bab18b46fb4523bdb97490b2a7e494ad94764
JatinS-xpdev <jatin.shetty@algosoftware.com>
2026-07-09 12:56:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 5173,
+    strictPort: false,
+    cors: true,
     proxy: {
-      '/api': 'http://localhost:3001'
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
     }
   }
 });
